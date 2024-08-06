41311f155245658a81b94913fd3327427bfac4ea
takles22 <99768500+takles22@users.noreply.github.com>
2024-08-07 00:24:34 +0300

    Update start_container.sh v2

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull takles22/hotel-app:latest
+docker pull mahertakles/hotel-app:latest
 
 # Run the Docker image as a container
-docker run -dit -p 80:80 takles22/hotel-app
+docker run -dit -p 80:80 mahertakles/hotel-app
 
